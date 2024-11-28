280e040b067fbef6d645098d1a3307b7488fef12
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-11-28 19:35:05 -0300

    feat: trabalho solicitado

---
+++ back_end/Trabalho_01/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Minha API muito louca mesmo!');
+});
+
+app.get('/hello', (req, res) => {
+  res.json('Hello world! Value prof! 😄')
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
